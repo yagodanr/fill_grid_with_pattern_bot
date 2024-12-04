e73df78e015ca911da914d4aec4b288f5667a54d
yagodanr <yagodanr@gmail.com>
2024-12-04 22:30:39 +0200

    Initial commit

---
+++ player1.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+    This is an example of a bot for the 3rd project.
+    ...a pretty bad bot to be honest -_-
+"""
+
+from logging import DEBUG, debug, getLogger
+
+# We use the debugger to print messages to stderr
+# You cannot use print as you usually do, the vm would intercept it
+# You can hovever do the following:
+#
+# import sys
+# print("HEHEY", file=sys.stderr)
+
+getLogger().setLevel(DEBUG)
+
+
+def parse_field_info():
+    """
+    Parse the info about the field.
+
+    However, the function doesn't do anything with it. Since the height of the field is
+    hard-coded later, this bot won't work with maps of different height.
+
+    The input may look like this:
+
+    Plateau 15 17:
+    """
+    l = input()
+    debug(f"Description of the field: {l}")
+
+
+def parse_field(player: int):
+    """
+    Parse the field.
+
+    First of all, this function is also responsible for determining the next
+    move. Actually, this function should rather only parse the field, and return
+    it to another function, where the logic for choosing the move will be.
+
+    Also, the algorithm for choosing the right move is wrong. This function
+    finds the first position of _our_ character, and outputs it. However, it
+    doesn't guarantee that the figure will be connected to only one cell of our
+    territory. It can not be connected at all (for example, when the figure has
+    empty cells), or it can be connected with multiple cells of our territory.
+    That's definitely what you should address.
+
+    Also, it might be useful to distinguish between lowecase (the most recent piece)
+    and uppercase letters to determine where the enemy is moving etc.
+
+    The input may look like this:
+
+        01234567890123456
+    000 .................
+    001 .................
+    002 .................
+    003 .................
+    004 .................
+    005 .................
+    006 .................
+    007 ..O..............
+    008 ..OOO............
+    009 .................
+    010 .................
+    011 .................
+    012 ..............X..
+    013 .................
+    014 .................
+
+    :param player int: Represents whether we're the first or second player
+    """
+    move = None
+    for i in range(16):
+        l = input()
+        debug(f"Field: {l}")
+        if move is None:
+            c = l.lower().find("o" if player == 1 else "x")
+            if c != -1:
+                move = i - 1, c - 4
+    assert move is not None
+    return move
+
+
+def parse_figure():
+    """
+    Parse the figure.
+
+    The function parses the height of the figure (maybe the width would be
+    useful as well), and then reads it.
+    It would be nice to save it and return for further usage.
+
+    The input may look like this:
+
+    Piece 2 2:
+    **
+    ..
+    """
+    l = input()
+    debug(f"Piece: {l}")
+    height = int(l.split()[1])
+    for _ in range(height):
+        l = input()
+        debug(f"Piece: {l}")
+
+
+def step(player: int):
+    """
+    Perform one step of the game.
+
+    :param player int: Represents whether we're the first or second player
+    """
+    move = None
+    parse_field_info()
+    move = parse_field(player)
+    parse_figure()
+    return move
+
+
+def play(player: int):
+    """
+    Main game loop.
+
+    :param player int: Represents whether we're the first or second player
+    """
+    while True:
+        move = step(player)
+        print(*move)
+
+
+def parse_info_about_player():
+    """
+    This function parses the info about the player
+
+    It can look like this:
+
+    $$$ exec p2 : [./player1.py]
+    """
+    i = input()
+    debug(f"Info about the player: {i}")
+    return 1 if "p1 :" in i else 2
+
+
+def main():
+    player = parse_info_about_player()
+    try:
+        play(player)
+    except EOFError:
+        debug("Cannot get input. Seems that we've lost ):")
+
+
+if __name__ == "__main__":
+    main()
